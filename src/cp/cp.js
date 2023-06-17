f46ae61e04c5a5cccdb8edd7ccb78afadc44ad24
Yulia Masliak <1985kozak@gmail.com>
2023-06-17 10:52:53 +0300

    feat: add child process solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,8 +1,12 @@
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-  // implement function spawnChildProcess that receives array of arguments args and creates child process from file script.js, passing these args to it. This function should create IPC-channel between stdin and stdout of master process and child process:
-  // child process stdin should receive input from master process stdin
-  // child process stdout should send data to master process stdout
+  const child = spawn('node', ['./files/script.js', ...args], {
+    stdio: 'pipe'
+  });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([2, 5, '10']);
